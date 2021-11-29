a85b53a3f3581a346787600225e5b90e257a2017
NatalyaSemenova <ambernou@gmail.com>
2021-11-29 19:54:23 +0700

    data in array last version

--- src/components/AstroPhoto/index.js
+++ src/components/AstroPhoto/index.js
@@ -34,9 +34,17 @@ export const AstroPhotoAPI = () => {
                     <button onClick={requestAstroInfo}>Request!</button>
                     {!!error && <h3>ERROR {error}</h3>}
 
-                    <h3>{astroInfo[0].title}</h3>
-                    <img src={astroInfo[0].url} />
-                    <h3>{astroInfo[0].description}</h3>
+                    {astroInfo.map((el) => (
+                        <div key={el.title}>
+                            <h3>{el.title}</h3>
+                            <img src={el.url} />
+                            <h3>{el.description}</h3>
+                        </div>
+                    ))}
+
+                    {/* <h3>{astroInfo[0].title}</h3> */}
+                    {/* <img src={astroInfo[0].url} />
+                    <h3>{astroInfo[0].description}</h3> */}
 
                     {/* <h3>"{astroInfo.title}"</h3>
                     <img src={astroInfo.url} width="400" alt="astro_photo" />
